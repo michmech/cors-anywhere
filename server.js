66a9ea47ea8f24a73c678d461561957927b60339
Michal Měchura <michmech@lexiconista.com>
2022-03-12 13:13:40 +0100

    whitelist tolstyslovar.com

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: [],
-  originWhitelist: ["http://localhost", "https://localhost"],
+  originWhitelist: ["http://localhost", "https://localhost", "http://www.tolstyslovar.com"],
   requireHeader: ['origin'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
